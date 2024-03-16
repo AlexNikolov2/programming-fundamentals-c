833fb2febfd87a42cb9808564322044fe10ba159
Aleks Nikolov <tcr.official.world@gmail.com>
2024-03-16 23:03:13 +0200

    opa opa

---
+++ session-1/ex2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    printf("Addition; num1 = %d, num2 = %d, sum1 = %d \n", 5,2,5+2);
+    printf("Subtraction; num1 = %d, num2 = %d, result1 = %d \n", 5,2, 5-2);
+    printf("Division; num1 = %d, num2 = %d, result = %d \n", 5,2, 5/2);
+    printf("Multiplication; num1 = %d, num2 = %d, result = %d \n", 5,2, 5*2);
+}
