c5ce93cc43ccb8c934982c10b6f65e394bd04c3e
Aleks Nikolov <tcr.official.world@gmail.com>
2024-03-18 16:20:51 +0200

    ok

---
+++ seminar-1/ex2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(){
+    int angle;
+    printf("Set angle: \n");
+    scanf("%d",&angle);
+    printf("Sine: %f \n", sin(angle));
+    printf("Cosine: %f \n", cos(angle));
+    printf("Tangent: %f \n", tan(angle));
+    //printf("Cotangent: %f \n", cotan(angle));
+
+}
