3ccf687ca198acccb75224830c03948e71a96cd4
Aleks Nikolov <tcr.official.world@gmail.com>
2024-03-25 12:31:00 +0200

    life is life

--- vlad-budinski-c-course/02-control-flow-exercise/ex1.c
+++ vlad-budinski-c-course/02-control-flow-exercise/ex1.c
@@ -2,5 +2,17 @@
 #include <stdbool.h>
 
 int main(){
-    
+    int num;
+    printf("Set num: \n");
+    scanf("%d", &num);
+
+    if(num > 10 && num < 100){
+        printf("%d is double digit num \n", num);
+    }
+    else if(num > 100 && num < 1000){
+        printf("%d is triple digit num \n", num);
+    }
+    else{
+        printf("who knows... :)");
+    }
 }
