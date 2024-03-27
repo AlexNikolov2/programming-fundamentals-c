8f93b2a84e2cb0e75c7503de5639e65b1049efeb
Aleks Nikolov <tcr.official.world@gmail.com>
2024-03-27 08:32:52 +0200

    okaaaayyy

--- lab-4/ex1.c
+++ lab-4/ex1.c
@@ -3,7 +3,18 @@
 
 int main(){
     int a, *b;
-    a = 10;
+    printf("set a: \n");
+    scanf("%d",&a);
+
+    printf("a = %d, &a = %d\n", a, &a);
+
     *b = &a;
-    printf("a = %d, b = %d\n",a,b);
+
+    printf("&b = %d\n", &b, *b);
+
+    b = 10;
+
+    printf("b = %d, &b = %d\n", b, &b);
+
+
 }
