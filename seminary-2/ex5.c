7f1aea777e80fcbd3f45dbdb807d7ca204c5bbe2
Aleks Nikolov <tcr.official.world@gmail.com>
2024-04-01 21:12:35 +0300

    ah

--- seminary-2/ex5.c
+++ seminary-2/ex5.c
@@ -1,3 +1,27 @@
-int calc_time(a,b,c){
+#include <stdio.h>
+
+void calc_time(int a, int b, int c){
+    int calced = a + b + c;
+
+    float min = (float)calced / 60;
+    int mins = (int)min;
+    int secs = (int)((min - mins) * 60);
+
+    printf("Moyenne: %d:%d\n", mins, secs);
+}
+int main(){
+
+    int a,b,c;
+
+    printf("set time a: \n");
+    scanf("%d",&a);
+    printf("set time b: \n");
+    scanf("%d",&b);
+    printf("set time c: \n");
+    scanf("%d",&c);
+
+    calc_time(a,b,c);
+
+    return 0;
     
 }
