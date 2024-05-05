8ab4e4760e49f996a0f58caee3a943fac04f8fd7
Aleks Nikolov <tcr.official.world@gmail.com>
2024-05-05 20:25:12 +0300

    workz

---
+++ exam-prep/ex7.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int findBiggest(int *ptr, int size){
+    int biggest = -5000;
+
+    for(int i = 0; i < size; i++){
+        if(ptr[i] >= biggest){
+            biggest = ptr[i];
+        }
+    }
+
+    printf("biggest: %d\n", biggest);
+}
+int main(){
+    int size;
+    printf("Set array size: \n");
+    scanf("%d",&size);
+
+    int arr[size];
+
+    for (int i = 0; i < size; i++){
+        int val;
+        printf("Set value: \n");
+        scanf("%d",&val);
+        arr[i] = val;
+    }
+
+    int *ptr_arr = arr;
+
+    findBiggest(ptr_arr, size);
+
+}
