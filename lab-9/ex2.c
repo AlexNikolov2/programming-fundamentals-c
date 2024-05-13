c21498ce112b19530da793c7b9d3fa27aca9eaf7
Aleks Nikolov <tcr.official.world@gmail.com>
2024-05-14 02:22:38 +0300

    all done

--- lab-9/ex2.c
+++ lab-9/ex2.c
@@ -25,6 +25,45 @@ int count_treatment(Treatment *arr, int size, char *name_param, char *diagnosis_
     }
 }
 
+Treatment* add_new_treatment(Treatment *arr, int *size) {
+    (*size)++;
+    arr = (Treatment*)realloc(arr, (*size) * sizeof(Treatment));
+
+    if (arr == NULL) {
+        printf("Memory reallocation failed");
+        return NULL;
+    }
+
+    printf("Enter Id: \n");
+    scanf("%d", &arr[*size - 1].id);
+
+    printf("Enter data: \n");
+    scanf("%s", arr[*size - 1].data);
+
+    printf("Enter name: \n");
+    scanf("%s", arr[*size - 1].name);
+
+    printf("Enter diagnosis: \n");
+    scanf("%s", arr[*size - 1].diagnosis);
+
+    return arr;
+}
+
+void write_text_file(Treatment *arr, int size, char *diagnosis){
+    FILE *text_file = fopen("illness.txt", "w");
+
+    if(text_file == NULL){
+        printf("Error opening file.");
+        exit(1);
+    }
+
+    for(int i = 0; i < size; i++){
+        if(strcmp(arr[i].diagnosis, diagnosis) == 0){
+            fprintf(text_file, "Stay of %s for %s: %s", arr[i].name, arr[i].diagnosis, arr[i].data);
+        }
+    }
+}
+
 int main(){
     int size;
     printf("Set array size: \n");
@@ -62,6 +101,11 @@ int main(){
     int treatments = count_treatment(history, size, name_param, diagnosis_param);
     printf("Treatments of %s with diagnosis %s: %d", name_param, diagnosis_param, treatments);
 
-    free(history);
+    history = add_new_treatment(history, &size);
+
+    if (history != NULL) {
+        free(history);
+    }
+
 }
 
